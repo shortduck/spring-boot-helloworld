560662a716492b66d6a81ae0c64596bd94995cbe
Bhurat <bhurata@michigan.gov>
2018-04-30 15:24:02 -0400

    first commit

---
+++ src/main/java/me/ankit/springboot/HelloWorld/HelloWorldApplication.java
@@ -0,0 +1,19 @@
+package me.ankit.springboot.HelloWorld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class HelloWorldApplication extends SpringBootServletInitializer  {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(HelloWorldApplication.class);
+	}
+	
+	public static void main(String[] args) {
+		SpringApplication.run(HelloWorldApplication.class, args);
+	}
+}
